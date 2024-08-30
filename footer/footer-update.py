6aca4e1643a881d0c10d08c5c142c04452be8e7c
Luis Antonio <119168200+ruisuan@users.noreply.github.com>
2024-08-30 18:39:26 +0000

    upd auto-footer

---
+++ footer/footer-update.py
@@ -0,0 +1,21 @@
+import os
+print(os.getcwd())
+
+footer = """
+<watermark-footer>
+<p align="center">
+  <a href="https://github.com/ruisuan/ruisuan/blob/stable/contribute.md">Contribute ☕</a> •
+</p>
+</watermark-footer>
+"""
+
+base_path = '.'
+
+for root, dirs, files in os.walk(base_path):
+    for file in files:
+        if file.endswith('.md'):
+            file_path = os.path.join(root, file)
+            with open(file_path, 'r+', encoding='utf-8') as f:
+                content = f.read()
+                if footer.strip() not in content:
+                    f.write(f'\n\n{footer.strip()}')
